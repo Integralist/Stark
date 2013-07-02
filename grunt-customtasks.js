f6a07b16437fc9ff7c859c347c074865c432c021
Mark McDonnell <mark.mcdx@gmail.com>
2013-07-02 10:39:05 +0100

    Add extensions into build script

--- grunt-customtasks.js
+++ grunt-customtasks.js
@@ -7,44 +7,73 @@ module.exports = function (grunt) {
     });
 
     grunt.registerTask('get-components', 'Parse each HTML file for components', function() {
-        var pages = grunt.file.expand('./*.html');
+        function parsePageName(page) {
+            return /\.\/(\w+)\.html/.exec(page)[1];
+        }
 
-        pages = pages.map(function(page) {
-            var content = grunt.file.read(page),
-                pattern = /data-component="([^"]+)"/gmi;
+        function getModules(page) {
+            var pattern = /data-component="([^"]+)"/gmi,
+                content = grunt.file.read(page);
 
-            content = content.match(pattern).map(function(module) {
+            return content.match(pattern).map(function(module) {
                 return 'components/' + module.split('"')[1] + '/component';
             });
+        }
 
-            return {
-                name: 'bootstrap-' + /\.\/([^.]+)/.exec(page)[1],
-                exclude: ['app'],
-                include: content,
-                insertRequire: content
-            };
-        });
-
-        // For full list of options see:
-        // https://github.com/jrburke/r.js/blob/master/build/example.build.js
-        requirejs_config = {
-            baseUrl: './',
-            dir: './release/',
-            paths: {
-                jquery: 'libs/jquery'
-            },
-            fileExclusionRegExp: /^\.|node_modules|Gruntfile|grunt-|libs|\.md|package.json/,
-            optimize: 'none',
-            removeCombined: true,
-            modules: pages,
-            onBuildRead: function (moduleName, path, contents) {
-                if (path.indexOf('bootstrap-') !== -1 || path.indexOf('/app.js') !== -1) {
-                    return '';
-                } else {
-                    return contents;
-                }
+        function getExtensions(page) {
+            var extensions = grunt.file.read('./bootstrap-' + parsePageName(page) + '.js'),
+                match = extensions.match(/app.use\('[a-z-.]+'\);/gmi);
+            
+            if (match) {
+                return match.map(function(){
+                    return 'extensions/' + /app.use\('([a-z-.]+)'\);/gmi.exec(extensions)[1] + '/extension';
+                });
             }
-        };
+        }
+
+        function buildConfig() {
+            var pages = grunt.file.expand('./*.html');
+
+            pages = pages.map(function(page) {
+                var modules    = getModules(page),
+                    extensions = getExtensions(page),
+                    toBeLoaded = modules;
+
+                if (extensions) {
+                    toBeLoaded = extensions.concat(modules);
+                }
+
+                return {
+                    name: 'bootstrap-' + /\.\/([^.]+)/.exec(page)[1],
+                    exclude: ['app'],
+                    include: toBeLoaded,
+                    insertRequire: toBeLoaded
+                };
+            });
+
+            // For full list of options see:
+            // https://github.com/jrburke/r.js/blob/master/build/example.build.js
+            requirejs_config = {
+                baseUrl: './',
+                dir: './release/',
+                paths: {
+                    jquery: 'libs/jquery'
+                },
+                fileExclusionRegExp: /^\.|node_modules|Gruntfile|grunt-|libs|\.md|package.json/,
+                optimize: 'none',
+                removeCombined: true,
+                modules: pages,
+                onBuildRead: function (moduleName, path, contents) {
+                    if (path.indexOf('bootstrap-') !== -1 || path.indexOf('/app.js') !== -1) {
+                        return '';
+                    } else {
+                        return contents;
+                    }
+                }
+            };
+        }
+
+        buildConfig();
 
         grunt.task.run('requirejs');
     });
