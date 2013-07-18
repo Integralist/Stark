3fbce00ab868dd1587cd95831368cec8e9ca1351
Mark McDonnell <mark.mcdx@gmail.com>
2013-07-18 11:12:18 +0100

    Create example component that publishes an event which is listened to by another component

--- components/contacts/component.js
+++ components/contacts/component.js
@@ -1,14 +1,46 @@
 define(['jquery'], function($) {
-    return {
-        createContactList: function() {
-            var contactsList    = document.createElement('select');
-                contactsList.id = 'js-contactsList';
+    var mediator;
 
-            console.log(contactsList);
-            console.log($);
+    return {
+        populateContactList: function(data) {
+            $.each(data, $.proxy(function(key, value) {
+                var option           = document.createElement('option');
+                    option.value     = value.name;
+                    option.innerHTML = value.name;
+
+                this.contactsList.appendChild(option);
+            }, this));
+
+            this.publish('contacts:updated', this.contactsList);
+        },
+
+        getContactData: function(){
+            $.ajax({
+                url: 'fixtures/contacts.json',
+                dataType: 'json',
+                success: $.proxy(this.populateContactList, this)
+            });
+        },
+
+        createContactList: function() {
+            this.contactsList    = document.createElement('select');
+            this.contactsList.id = 'js-contactsList';
+            this.contactsList.setAttribute('data-component', 'contacts');
+            
+            $(this.container).replaceWith(this.contactsList);
+
+            this.getContactData();
+        },
+
+        bindEvents: function() {
+            //
         },
 
         init: function() {
+            mediator = window.app.mediator;
+            mediator.wrap(this);
+
+            this.bindEvents();
             this.container = window.app.components.contacts;
             this.createContactList();
         }
--- components/world/component.js
+++ components/world/component.js
@@ -1,9 +1,18 @@
-define({
-    init: function() {
-        console.log('world component initialised');
-        
-        if (window.app.mediator) {
-            console.log('app.mediator', app.mediator); // will be undefined for bootstrap-index.js
+define(function(){
+    var mediator;
+
+    return {
+        init: function() {
+            mediator = window.app.mediator;
+            console.log('world component initialised');
+            
+            if (mediator) {
+                mediator.subscribe('contacts:updated', function(data) {
+                    console.log('No idea why the "world" component needs to know about the contacts component, but meh here it is:', data);
+                });
+
+                console.log('app.mediator', app.mediator); // will be undefined for bootstrap-index.js
+            }
         }
-    }
+    };
 });
